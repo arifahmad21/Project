4c8dad536b452242d0d84f1902046b721ef6d041
arifahmad21 <arifahmad0203@gmail.com>
2023-10-01 20:45:21 -0700

    update1

--- login/script.js
+++ login/script.js
@@ -1,10 +1,13 @@
-const passwordInput = document.getElementById("password");
-const showPasswordIcon = document.getElementById("show-password");
+const passwordInput = document.getElementById('password');
+const showPassword = document.getElementById('toggle');
 
-showPasswordIcon.addEventListener("click", function () {
-  if (passwordInput.type === "password") {
-    passwordInput.type = "text";
-  } else {
-    passwordInput.type = "password";
+function showHide(){
+  if(passwordInput.type === 'password'){
+    passwordInput.setAttribute('type', 'text');
+    showPassword.classList.add('hide')
+  }else {
+    passwordInput.setAttribute('type', 'password');
+    showPassword.classList.remove('hide')
   }
-});
+}
+
