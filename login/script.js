60ffcd6aa59334e6f6ab81cbf8b8c4e6466d9f47
arifahmad21 <arifahmad0203@gmail.com>
2023-10-07 14:42:17 -0700

    commited

--- login/script.js
+++ login/script.js
@@ -1,13 +1,12 @@
-const passwordInput = document.getElementById('password');
-const showPassword = document.getElementById('toggle');
+const passwordInput = document.getElementById("password");
+const showPassword = document.getElementById("toggle");
 
-function showHide(){
-  if(passwordInput.type === 'password'){
-    passwordInput.setAttribute('type', 'text');
-    showPassword.classList.add('hide')
-  }else {
-    passwordInput.setAttribute('type', 'password');
-    showPassword.classList.remove('hide')
+function showHide() {
+  if (passwordInput.type === "password") {
+    passwordInput.setAttribute("type", "text");
+    showPassword.classList.add("hide");
+  } else {
+    passwordInput.setAttribute("type", "password");
+    showPassword.classList.remove("hide");
   }
 }
-
